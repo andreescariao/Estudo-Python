4b3e0bf713e26c929847be56ddaefc1eb2da0b67
André Escarião <andre.escariao1@gmail.com>
2022-07-28 22:08:35 -0300

    Create random_casting.py

---
+++ Sem_01/random_casting.py
@@ -0,0 +1,18 @@
+# Random e operação casting
+
+# Operação de casting
+num_i=10
+num_f=5.2
+x=num_i
+
+print("Valor: "+str(x)+" - Tipo: "+str(type(x)))
+
+# Conversao de tipo
+x=int(num_f)
+print("Valor: "+str(x)+" - Tipo: "+str(type(x)))
+
+# Random
+import random
+num_r=random.randrange(0,59)
+x=num_r
+print("Valor: "+str(x)+ " - Tipo: "+ str(type(x)))
