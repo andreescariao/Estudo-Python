b6aac93646dca369b811f0d4bf3e037fb944cee6
André Escarião <andre.escariao1@gmail.com>
2022-07-29 11:09:02 -0300

    Create Boolean.py

---
+++ Sem_01/Boolean.py
@@ -0,0 +1,24 @@
+#Boolean
+from operator import truediv
+
+#Bool em True ou False
+aula=True
+print(aula)
+
+#Bool em texto
+aula=""
+if aula:
+    print("Possui Texto")
+else:
+    print("Vazio")
+#Bool em expressões
+aula=0
+if aula:
+    print("Possui Número")
+else:
+    print("Vazio")
+aula=1
+if aula:
+    print("Possui Número")
+else:
+    print("Vazio")
