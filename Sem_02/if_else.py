22a4026b44f0ba1f9c08fa4f936c6cfb81925502
André Escarião <andre.escariao1@gmail.com>
2022-07-29 22:13:56 -0300

    Update if_else.py

--- Sem_02/if_else.py
+++ Sem_02/if_else.py
@@ -1,1 +1,35 @@
+# Estrutura If Else
+a=True;b=False
+if a:
+    print("Verdadeiro")
+else:
+    print("Falso")
+if b:
+    print("Verdadeiro")
+else:
+    print("Falso")
 
+# Com operações
+a=10;b=5;res=0;op="+"
+if op=="+":
+    res=a+b
+elif op=="-":
+    res=a-b
+elif op=="*":
+    res=a*b
+elif op=="/":
+    res=a/b
+else:
+    print("Operador inválido")
+print("Operação: "+op+"\nresultado: "+str(res))
+
+#---------------------------------------------------
+
+clima="sol";luga="";dinheiro=650
+
+if clima=="sol" or (dinheiro>=300 and dinheiro<=500):
+    lugar="clube"
+else:
+    lugar="cinema"
+
+print("\nvou ao "+lugar)
